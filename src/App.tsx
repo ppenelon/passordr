147130d677fc31a4ef005e99264b12941f9f3fc1
Pierre Penelon <ppenelon@kiliba.com>
2023-02-04 18:54:53 +0100

    Add Edit/Save/Cancel (mock) with buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useReducer, useRef, useState } from 'react';
+import React, { useMemo, useReducer, useRef, useState, Fragment } from 'react';
 import classNames from 'classnames';
 import './App.css';
 
@@ -7,29 +7,44 @@ const dbHint = "Hello\nBoys";
 
 function App() {
   const [editMode, setEditMode] = useState(false);
-  
+
   const [hint, setHint] = useState(dbHint);
   const hintLinesCount = useMemo(() => hint.split(/\r\n|\r|\n/).length, [hint]);
 
   const [lines, setLines] = useState([...dbLines, { name: "", outdated: false }]);
   const linesInputRef = useRef<(HTMLInputElement | null)[]>([]);
 
-  const [savedContent, updateSavedContent] = useReducer(() => ({ hint: hint, lines: [...lines]}), { hint: hint, lines: [...lines]});
+  const [savedContent, updateSavedContent] = useReducer(() => ({ hint: hint, lines: [...lines] }), { hint: hint, lines: [...lines] });
   const editedLinesStatus = useMemo(() => lines.map((line, i) => {
-    if(i === savedContent.lines.length - 1 && lines[i].name) {
+    if (i === savedContent.lines.length - 1 && lines[i].name) {
       return 'added';
-    } else if(!savedContent.lines[i]) {
+    } else if (!savedContent.lines[i]) {
       return 'added';
-    } else if(savedContent.lines[i].outdated && !line.outdated) {
+    } else if (savedContent.lines[i].outdated && !line.outdated) {
       return 'added';
-    } else if(!savedContent.lines[i].outdated && line.outdated) {
+    } else if (!savedContent.lines[i].outdated && line.outdated) {
       return 'outdate';
-    } else if(savedContent.lines[i].name !== line.name) {
+    } else if (savedContent.lines[i].name !== line.name) {
       return 'update';
     }
     return false;
   }), [lines, savedContent]);
 
+  function goEditMode() {
+    setEditMode(true);
+    updateSavedContent();
+  }
+
+  function goSave() {
+    setEditMode(false);
+  }
+
+  function goCancel() {
+    setHint(savedContent.hint);
+    setLines(savedContent.lines);
+    setEditMode(false);
+  }
+  
   function toggleLineOutdated(lineIndex: number) {
     setLines(lines => {
       const newLines = [...lines];
@@ -37,12 +52,12 @@ function App() {
       return newLines;
     });
   }
-  
+
   function setLineContent(index: number, value: string) {
     setLines(lines => {
       const newLines = [...lines];
       newLines[index] = { ...newLines[index], name: value };
-      if(index === lines.length - 1) {
+      if (index === lines.length - 1) {
         newLines.push({ name: '', outdated: false });
       }
       return newLines;
@@ -62,7 +77,7 @@ function App() {
   }
 
   function handleKey(event: React.KeyboardEvent<HTMLInputElement>, lineIndex: number) {
-    switch(event.key) {
+    switch (event.key) {
       case "Enter":
       case "ArrowDown":
         jumpToInput(lineIndex + 1);
@@ -71,7 +86,7 @@ function App() {
         jumpToInput(lineIndex - 1);
         break;
       case "Backspace":
-        if(!lines[lineIndex].name.length && lineIndex >= savedContent.lines.length) {
+        if (!lines[lineIndex].name.length && lineIndex >= savedContent.lines.length) {
           jumpToInput(lineIndex - 1);
           deleteLine(lineIndex);
           event.preventDefault();
@@ -86,15 +101,19 @@ function App() {
       <div className="header">
         {/* Title */}
         <div className="title">
-          Passordr
+          passordr
         </div>
 
         {/* Toogle edit mode */}
-        <div className="edit-mode">
-          <label>
-            Edit mode
-            <input type="checkbox" checked={editMode} onChange={e => {setEditMode(e.target.checked); updateSavedContent()}} />
-          </label>
+        <div className="edit-toggle">
+          {
+            editMode ?
+              (<Fragment>
+                <button onClick={e => goCancel()}>CANCEL</button>
+                <button onClick={e => goSave()}>SAVE</button>
+              </Fragment>) :
+              (<button onClick={e => goEditMode()}>EDIT</button>)
+          }
         </div>
       </div>
 
@@ -110,10 +129,10 @@ function App() {
 
       {/* Lines / Services */}
       <div className={classNames("lines", { 'edit-mode': editMode })}>
-        {lines.map((line, i) => 
-          <div className={classNames("line", { outdated: line.outdated, new: i === lines.length - 1}, editMode && editedLinesStatus[i] && ["edited", editedLinesStatus[i]])} key={i}>
+        {lines.map((line, i) =>
+          <div className={classNames("line", { outdated: line.outdated, new: i === lines.length - 1 }, editMode && editedLinesStatus[i] && ["edited", editedLinesStatus[i]])} key={i}>
             <div className="index" onClick={e => editMode && toggleLineOutdated(i)}>{i}</div>
-            <input 
+            <input
               className="title"
               ref={el => linesInputRef.current[i] = el}
               value={line.name}
