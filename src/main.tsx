0d5157f9e3031f6a131bafd587d5f52283329f96
Pierre Penelon <penelon.pierre@gmail.com>
2023-06-01 23:19:22 +0200

    Debug option: Clear storage

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./main.css";
 
+// Debug options
+const params = new URLSearchParams(window.location.search);
+if (params.get("reset_passordr")) {
+  if (confirm("Are you sure to clear storage ?")) {
+    localStorage.clear();
+    params.delete("reset_passordr");
+    window.location.search = params.toString();
+  }
+}
+
+// Render React
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
